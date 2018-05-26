704c047f3ce4732efb3e9fd5caacaca9c503e457
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-05-26 17:54:40 +0200

    Adding a menu for mobile navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react';
 
 import './Navbar.css';
 
-const Navbar = () => {
-  return (
-    <nav className="navbar is-transparent is-fixed-top navbar__container" aria-label="main navigation">
-      <div className="navbar-start">
-        <a className="navbar-item logo" href="/">
-          On My Travel.
-        </a>
-      </div>
-      <div className="container">
-      <div className="navbar-end">        
-          <a className="navbar-item" href="/log-in">
-            Log-in
+class Navbar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+  }
+
+  render() {
+    return (
+      <nav className="navbar is-dark navbar__container" aria-label="main navigation">
+  
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            On My Travel. {this.state.isActive}
           </a>
-          <a className="navbar-item" href="/profile">
-            Mon profile
-          </a>
+          <div className="navbar-burger burger" onClick={() => this.setState({ isActive: !this.state.isActive })}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
-      </div>
-    </nav>
-  )
+  
+        <div className={"navbar-menu" + (this.state.isActive ? 'is-active' : '')}>
+          <div className="navbar-end">
+            <a className="navbar-item" href="/log-in">
+              Log-in
+            </a>
+            <a className="navbar-item" href="/profile">
+              Mon profile
+            </a>
+          </div>
+        </div>
+      </nav >
+    )
+  }
 }
 
 export default Navbar
