d6c3279b619b07efdec2d19f2194b13d2e4f2e0f
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-06-16 17:19:22 +0200

    Removing the <Navbar> container

--- src/Router.js
+++ src/Router.js
@@ -15,14 +15,11 @@ import OfflinePage from './containers/OfflinePage';
 import ChartePage from './containers/ChartePage';
 import Homepage from './containers/Homepage';
 
-import Navbar from './components/Navbar';
-
 const AppRouter = ({ store, history, persistor }) => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <ConnectedRouter history={history}>
           <div id="router">
-            <Navbar />
             <Route exact path="/" component={OfflinePage} />
             <Route exact path="/log-in" component={LoginPage} />
             <Route exact path="/charte" component={ChartePage} />
