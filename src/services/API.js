23ad52b2de5f15d610cc82b8d43f5a843b1ba6f2
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-06-27 09:37:40 +0200

    Setting .env configuration

--- src/services/API.js
+++ src/services/API.js
@@ -1,4 +1,7 @@
 import APIClient from './APIClient';
 import HttpClient from './HttpClient'
 
-export default new APIClient({ httpClient: new HttpClient({ baseUrl: 'http://localhost:3001' }) });
+console.log(process.env.REACT_APP_API_URL);
+export default new APIClient({
+  httpClient: new HttpClient({ baseUrl: process.env.REACT_APP_API_URL })
+});
