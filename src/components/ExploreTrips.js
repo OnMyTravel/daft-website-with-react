fe102004230ebecd9a42978e3959cf5541ee3944
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-10-28 11:21:43 +0100

    [FEATURE] Creating a form to select the travel

--- src/components/ExploreTrips.js
+++ src/components/ExploreTrips.js
@@ -5,9 +5,19 @@ import '../styles/ExploreTrips.css';
 const ExploreTrips = () => {
 
   return (
-  <div className="timeline__wrapper"> 
-    Explore trips
-  </div>
+    <section className="hero is-fullheight exploreTrip__frame">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title is-1">Rejoindre une aventure ...</h1>
+          
+          <div className="code_form">
+            <label for="travelId">Pour accéder à un voyage, saisissez son code ci-dessous</label>
+            <input className="input" id="travelId"/>
+            <a className="button is-primary">C'est parti</a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
